67b9523711f3ebb881d11c2f5edfccbe585aac64
adri <adrian.g.job@gmail.com>
2026-02-08 19:41:39 +0100

    refactor added abstract to BaseEntity

--- src/infrastructure/base/urm/BaseEntity.ts
+++ src/infrastructure/base/urm/BaseEntity.ts
@@ -1,6 +1,6 @@
 import type { BaseEntityInterface } from "../interfaces/urm/BaseEntityInterface";
 
-export class BaseEntity implements BaseEntityInterface {
+export abstract class BaseEntity implements BaseEntityInterface {
   id: string;
   constructor(id: string) {
     this.id = id;
