f3e25cacc02f715ed8a01a62cd57622558d1f997
adri <adrian.g.job@gmail.com>
2026-03-31 01:00:47 +0200

    added gobackbutton to materials and monster main views

--- src/application/features/ZeldaBOTW/Materials/views/MaterialsView.tsx
+++ src/application/features/ZeldaBOTW/Materials/views/MaterialsView.tsx
@@ -1,15 +1,15 @@
 import MaterialsContainer from "../components/containers/MaterialsContainer";
 import PageTitle from "../../../../shared/ui/PageTitle";
+import GoBackButton from "../../../../shared/components/ui/GoBackButton";
 
 const MaterialsView: React.FC = () => {
-
   return (
     <div className="m-4">
       <PageTitle text="Monstruos de Zelda BOTW" />
+      <GoBackButton route={"/zelda/"}></GoBackButton>
       <MaterialsContainer></MaterialsContainer>
     </div>
   );
 };
 
 export default MaterialsView;
-
--- src/application/features/ZeldaBOTW/Monsters/views/MonstersView.tsx
+++ src/application/features/ZeldaBOTW/Monsters/views/MonstersView.tsx
@@ -1,11 +1,13 @@
 import MonstersContainer from "../components/containers/MonstersContainer";
 import PageTitle from "../../../../shared/ui/PageTitle";
+import GoBackButton from "../../../../shared/components/ui/GoBackButton";
 
 const MonstersView: React.FC = () => {
 
   return (
     <div className="m-4">
       <PageTitle text="Monstruos de Zelda BOTW" />
+      <GoBackButton route={"/zelda/"}></GoBackButton>
       <MonstersContainer></MonstersContainer>
     </div>
   );
